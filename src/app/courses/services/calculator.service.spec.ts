3d22c385b6045d59195101bbd3cef95ecfc08970
Agustin Garcia <a.garcia@pnptc.com>
2021-09-24 09:03:28 +0200

    Best Practices

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -23,13 +23,14 @@ describe('CalculatorService', () => {
   });
 
   it('should add two numbers', () => {
-    
+    console.log('add test');
     const result = calculator.add(2, 2);
     expect(result).toBe(4);
     expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should substract two numbers', () => {
+    console.log('substract test');
     const result = calculator.subtract(2, 2);
     expect(result).toBe(0, 'unexpected substraction result');
     expect(loggerSpy.log).toHaveBeenCalledTimes(1);
