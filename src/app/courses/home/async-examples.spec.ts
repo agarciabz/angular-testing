3eb65fdfc4544b600a435ec0ff035173570f7f8f
Agustin Garcia <a.garcia@pnptc.com>
2021-09-26 18:24:08 +0200

    Asynchronous Testing - Introduction to microtasks

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,6 +1,6 @@
 import { fakeAsync, tick } from '@angular/core/testing';
 
-fdescribe('Async Testing Examples', () => {
+describe('Async Testing Examples', () => {
   it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
     let test = false;
     setTimeout(() => {
@@ -18,6 +18,31 @@ fdescribe('Async Testing Examples', () => {
       test = true;
       expect(test).toBeTruthy();
     }, 1000);
-    tick(1000);
+    tick(1000); // or flush()
   }));
+
+  fit('Asynchronous test example - plan Promise', () => {
+    let test = false;
+    /**
+     * Run and see order of logging.
+     * Microtasks (Promise) run before tasks (setTimeout).
+     */
+    console.log('Creating promise');
+    setTimeout(() => {
+      console.log('setTimeout() first callback triggered');
+    });
+    setTimeout(() => {
+      console.log('setTimeout() second callback triggered');
+    });
+    Promise.resolve().then(() => {
+      console.log('Promise first then() evaluated successfully');
+      return Promise.resolve();
+    }).then(() => {
+      console.log('Promise second then() evaluated successfully');
+      test = true;
+    });
+    console.log('Running test assertiosn - Promise')
+
+    expect(test).toBeTruthy();
+  });
 });
