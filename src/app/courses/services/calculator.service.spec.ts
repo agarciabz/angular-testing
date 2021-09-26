feca5c39f9884ea29dbcce3216200a2c0f015010
Agustin Garcia <a.garcia@pnptc.com>
2021-09-26 13:25:46 +0200

    Components - Container components

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -8,7 +8,6 @@ describe('CalculatorService', () => {
   let loggerSpy: any;
   
   beforeEach(() => {
-    console.log('Calling beforeEach');
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
     TestBed.configureTestingModule({
       providers: [
@@ -23,14 +22,12 @@ describe('CalculatorService', () => {
   });
 
   it('should add two numbers', () => {
-    console.log('add test');
     const result = calculator.add(2, 2);
     expect(result).toBe(4);
     expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should substract two numbers', () => {
-    console.log('substract test');
     const result = calculator.subtract(2, 2);
     expect(result).toBe(0, 'unexpected substraction result');
     expect(loggerSpy.log).toHaveBeenCalledTimes(1);
